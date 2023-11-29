957408be7a593e7e5ff9ab1c09f5bd383fa58812
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-11-30 01:30:23 +0200

    added BookService

--- src/main/java/mate/academy/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/bookstore/repository/impl/BookRepositoryImpl.java
@@ -9,7 +9,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class BookRepositoryImpl implements BookRepository {
     private final SessionFactory sessionFactory;
 
