7ced5b8479446d169992c8ca8ddf299c8d0e9f38
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-11-29 19:56:25 +0200

    added BookRepository

--- src/main/java/mate/academy/bookstore/model/Book.java
+++ src/main/java/mate/academy/bookstore/model/Book.java
@@ -1,9 +1,5 @@
 package mate.academy.bookstore.model;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +7,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "books")
