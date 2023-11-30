96be7534e51d129d9cca5a39f28380fb4752b5c8
Krasnov-Maksim <106538223+Krasnov-Maksim@users.noreply.github.com>
2023-11-30 14:48:09 +0200

    solved task "Create a repo" in module "Spring Boot Intro" (#1)
    * configured database parameters in application.properties
    
    * added Book entity
    
    * added BookRepository
    
    * added BookService

--- src/main/java/mate/academy/bookstore/BookStoreApplication.java
+++ src/main/java/mate/academy/bookstore/BookStoreApplication.java
@@ -1,13 +1,36 @@
 package mate.academy.bookstore;
 
+import java.math.BigDecimal;
+import mate.academy.bookstore.model.Book;
+import mate.academy.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+
+    @Autowired(required = true)
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Book book = new Book();
+            book.setAuthor("The Best Author ");
+            book.setCoverImage("image.png");
+            book.setDescription("book description..");
+            book.setIsbn("uwrt-12345");
+            book.setPrice(BigDecimal.valueOf(100.5));
+            book.setTitle("Book Title");
+            bookService.save(book);
+            System.out.println("\n" + book);
+        };
+    }
 }
