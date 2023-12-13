065ec0acf493915bb2cc9514cb19476ad4ae9a68
Krasnov-Maksim <106538223+Krasnov-Maksim@users.noreply.github.com>
2023-12-13 17:57:45 +0200

    Solved criteria query in spring boot (#4)
    * add 02-populate-books-table-with-data.yaml
    
    * solved criteria-query-in-spring-boot
    
    * solved criteria-query-in-spring-boot

--- src/main/java/mate/academy/bookstore/repository/JpaBookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/JpaBookRepository.java
@@ -2,6 +2,8 @@ package mate.academy.bookstore.repository;
 
 import mate.academy.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface JpaBookRepository extends JpaRepository<Book, Long> {
+public interface JpaBookRepository extends JpaRepository<Book, Long>,
+        JpaSpecificationExecutor<Book> {
 }
