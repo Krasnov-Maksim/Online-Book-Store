8a4fc4461776547e7bb161567678ca12731017a9
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-12-19 12:42:51 +0200

    removed custom validator

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -4,9 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
-import mate.academy.bookstore.validation.annotation.CreateBookRequestDtoValidation;
 
-@CreateBookRequestDtoValidation
 public record CreateBookRequestDto(
         @NotBlank()
         String title,
