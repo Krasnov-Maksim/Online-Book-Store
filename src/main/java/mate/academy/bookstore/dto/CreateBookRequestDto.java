5ab0a12e9d89b44fd3ce55e030a3f7bbc7dddc11
Krasnov-Maksim <106538223+Krasnov-Maksim@users.noreply.github.com>
2023-12-20 16:13:32 +0200

    Solved global exception handler and data validation (#5)
    * add custom data validation
    
    * add CustomGlobalExceptionHandler
    
    * removed custom validator
    
    * added validation for isbn, description.
    added timestamp into CustomGlobalExceptionHandler methods.
    
    * added custom validation annotation for isbn, fixed errors

--- src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/CreateBookRequestDto.java
@@ -1,12 +1,21 @@
 package mate.academy.bookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
+import mate.academy.bookstore.validation.Isbn;
 
 public record CreateBookRequestDto(
+        @NotBlank
         String title,
+        @NotBlank
         String author,
+        @Isbn
         String isbn,
+        @PositiveOrZero
         BigDecimal price,
+        @Size(min = 3)
         String description,
         String coverImage) {
 }
