c97bdb6120cb2c27d06165ea659007e7f5183b5d
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-01-07 15:38:16 +0200

    add stubs for AuthenticationController & UserService

---
+++ src/main/java/mate/academy/bookstore/controller/AuthenticationController.java
@@ -0,0 +1,24 @@
+package mate.academy.bookstore.controller;
+
+import lombok.RequiredArgsConstructor;
+import mate.academy.bookstore.dto.user.UserRegistrationRequestDto;
+import mate.academy.bookstore.dto.user.UserResponseDto;
+import mate.academy.bookstore.exception.RegistrationException;
+import mate.academy.bookstore.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthenticationController {
+    private final UserService userService;
+
+    @PostMapping("/registration")
+    public UserResponseDto register(UserRegistrationRequestDto request)
+            throws RegistrationException {
+        // FIXME: -> null
+        return null;
+    }
+}
---
+++ src/main/java/mate/academy/bookstore/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package mate.academy.bookstore.service;
+
+import mate.academy.bookstore.dto.user.UserRegistrationRequestDto;
+import mate.academy.bookstore.dto.user.UserResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto requestDto) {
+        // FIXME: -> null
+        return null;
+    }
+}
