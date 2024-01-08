6172237bbeebe0a3de24711313095528fb0ea2fc
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-01-08 19:44:14 +0200

    Add UserMapper

---
+++ src/main/java/mate/academy/bookstore/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package mate.academy.bookstore.mapper;
+
+import mate.academy.bookstore.dto.user.UserRegistrationRequestDto;
+import mate.academy.bookstore.dto.user.UserResponseDto;
+import mate.academy.bookstore.model.User;
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.NullValueCheckStrategy;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        implementationPackage = "<PACKAGE_NAME>.impl"
+)
+public interface UserMapper {
+    UserResponseDto toDto(User user);
+
+    User toModel(UserRegistrationRequestDto requestDto);
+}
