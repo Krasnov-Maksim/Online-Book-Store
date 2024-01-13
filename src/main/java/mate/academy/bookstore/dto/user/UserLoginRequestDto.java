23d27c7fef46fe3d7128955ee0d7c05b90fc3018
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-01-13 15:59:30 +0200

    add AuthenticationService, UserLoginRequestDto, UserLoginResponseDto

---
+++ src/main/java/mate/academy/bookstore/dto/user/UserLoginRequestDto.java
@@ -0,0 +1,14 @@
+package mate.academy.bookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UserLoginRequestDto(
+        @NotNull
+        @Email
+        String email,
+        @NotNull
+        @Size(min = 8, message = "Password must contain at least 8 characters")
+        String password) {
+}
