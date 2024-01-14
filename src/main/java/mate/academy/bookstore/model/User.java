590b811d2e0fd6017ca0d8cbaeae8c1ddc6e1c3c
Krasnov-Maksim <106538223+Krasnov-Maksim@users.noreply.github.com>
2024-01-14 15:50:04 +0200

    Solved spring boot jwt (#9)
    * add JWT dependencies
    
    * add AuthenticationService, UserLoginRequestDto, UserLoginResponseDto
    
    * add methode 'login' to AuthenticationController
    
    * update application.properties
    
    * solved spring-boot-jwt

--- src/main/java/mate/academy/bookstore/model/User.java
+++ src/main/java/mate/academy/bookstore/model/User.java
@@ -53,7 +53,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return roles;
     }
 
     @Override
