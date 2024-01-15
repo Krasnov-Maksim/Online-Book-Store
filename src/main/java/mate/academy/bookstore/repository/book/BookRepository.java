c7d4d832e956177c581e9dd387c55750131fd7cb
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-01-15 15:20:46 +0200

    add methode findAllByCategoryId into BookRepository

--- src/main/java/mate/academy/bookstore/repository/book/BookRepository.java
+++ src/main/java/mate/academy/bookstore/repository/book/BookRepository.java
@@ -1,9 +1,12 @@
 package mate.academy.bookstore.repository.book;
 
+import java.util.List;
 import mate.academy.bookstore.model.Book;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
+    List<Book> findAllByCategoryId(Long categoryId, Pageable pageable);
 }
