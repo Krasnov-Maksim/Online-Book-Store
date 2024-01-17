cd592b6ef7dddca5196b2b3e92397bfec9172e85
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-01-17 21:36:52 +0200

    add ShoppingCart, CartItem

---
+++ src/main/java/mate/academy/bookstore/model/ShoppingCart.java
@@ -0,0 +1,37 @@
+package mate.academy.bookstore.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE shopping_carts SET is_Deleted=true WHERE id=?")
+@Where(clause = "is_deleted=false")
+@Table(name = "shopping_carts")
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @MapsId
+    private User user;
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.REMOVE)
+    private Set<CartItem> cartItems = new HashSet<>();
+    @Column(nullable = false)
+    private boolean isDeleted = false;
+}
