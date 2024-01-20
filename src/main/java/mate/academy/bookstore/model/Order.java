c4a26c2db669890db822db064248ff290f39d568
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2024-01-20 17:08:56 +0200

    add 'Order' & 'OrderItem'

---
+++ src/main/java/mate/academy/bookstore/model/Order.java
@@ -0,0 +1,56 @@
+package mate.academy.bookstore.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Setter
+@SQLDelete(sql = "UPDATE orders SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted = false")
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+    @Column(nullable = false)
+    private BigDecimal total;
+    @Column(nullable = false)
+    private LocalDateTime orderDate;
+    @Column(nullable = false)
+    private String shippingAddress;
+    @OneToMany(mappedBy = "order", cascade = CascadeType.REMOVE)
+    private Set<OrderItem> orderItems = new HashSet<>();
+    private boolean isDeleted = false;
+
+    public enum Status {
+        COMPLETED,
+        PENDING,
+        DELIVERED,
+        PROCESSING
+    }
+}
--- src/main/java/mate/academy/bookstore/model/OrderItem.java
+++ src/main/java/mate/academy/bookstore/model/OrderItem.java
@@ -9,29 +9,32 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.math.BigDecimal;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
-@Entity
 @Getter
 @Setter
-@SQLDelete(sql = "UPDATE cart_items SET is_deleted = true WHERE id = ?")
+@SQLDelete(sql = "UPDATE order_items SET is_deleted = true WHERE id=?")
 @Where(clause = "is_deleted = false")
-@Table(name = "cart_items")
-public class CartItem {
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
-    @JoinColumn(name = "shopping_cart_id")
-    private ShoppingCart shoppingCart;
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
     @OneToOne
     @JoinColumn(name = "book_id", nullable = false)
     private Book book;
     @Column(nullable = false)
     private Integer quantity;
+    @Column(nullable = false)
+    private BigDecimal price;
     @Column(nullable = false)
     private boolean isDeleted = false;
 }
